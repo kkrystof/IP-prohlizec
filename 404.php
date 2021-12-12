c20cb948a8c986aaef661475f623baacccfa0233
kkrystof <krystofus.kulhanek@gmail.com>
2021-12-12 19:28:34 +0100

    secound commit

--- 404.php
+++ 404.php
@@ -1,19 +1,7 @@
 <?php
 header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
-//include("notFound.php");
 
-//Not Found
-//The requested URL /test.php was not found on this server.
-//
-//That's because the web server doesn't send that page when PHP returns a 404 code (at least Apache doesn't). PHP is responsible for sending all its own output. So if you want a similar page, you'll have to send the HTML yourself, e.g.:
-//
-//<?php
-//header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
-//include("notFound.php");
-//
-//You could configure Apache to use the same page for its own 404 messages, by putting this in httpd.conf:
-//
-//ErrorDocument 404 /notFound.php
+
 ?>
 <!doctype html>
 <html lang="cs">
